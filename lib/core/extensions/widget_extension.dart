0977d9785a35371561560dc92103d199400e2e5a
Jewel <ylingzhu2022@gmail.com>
2024-11-29 11:21:54 +0800

    feat: add WidgetExtension

---
+++ lib/core/extensions/widget_extension.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+extension WidgetExtension on Widget {
+  InkWell onInkTap(final GestureTapCallback? onTap) => InkWell(
+        onTap: onTap,
+        child: this,
+      );
+
+  Widget get left {
+    return Align(alignment: Alignment.centerLeft, child: this);
+  }
+
+  Widget get right {
+    return Align(alignment: Alignment.centerRight, child: this);
+  }
+
+  Widget get center {
+    return Align(alignment: Alignment.center, child: this);
+  }
+
+  Widget get topLeft {
+    return Align(alignment: Alignment.topLeft, child: this);
+  }
+
+  Widget get topRight {
+    return Align(alignment: Alignment.topRight, child: this);
+  }
+
+  Widget get topCenter {
+    return Align(alignment: Alignment.topCenter, child: this);
+  }
+
+  Widget get bottomCenter {
+    return Align(alignment: Alignment.bottomCenter, child: this);
+  }
+
+  Widget pt(double distance) => Padding(
+        padding: EdgeInsets.only(top: distance),
+        child: this,
+      );
+  Widget pl(double distance) => Padding(
+        padding: EdgeInsets.only(left: distance),
+        child: this,
+      );
+  Widget pr(double distance) => Padding(
+        padding: EdgeInsets.only(right: distance),
+        child: this,
+      );
+  Widget pb(double distance) => Padding(
+        padding: EdgeInsets.only(bottom: distance),
+        child: this,
+      );
+  Widget px(double distance) => Padding(
+        padding: EdgeInsets.symmetric(horizontal: distance),
+        child: this,
+      );
+  Widget py(double distance) => Padding(
+        padding: EdgeInsets.symmetric(vertical: distance),
+        child: this,
+      );
+}
