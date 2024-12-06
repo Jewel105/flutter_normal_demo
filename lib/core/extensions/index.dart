044b0f28e757fbe2a96a11f31c5e43547104b781
Jewel <ylingzhu2022@gmail.com>
2024-12-06 10:14:43 +0800

    feat: add StringExtension

--- lib/core/extensions/index.dart
+++ lib/core/extensions/index.dart
@@ -1,1 +1,4 @@
 export 'context_extension.dart';
+export 'string_extension.dart';
+export 'text_extension.dart';
+export 'widget_extension.dart';
--- lib/core/extensions/string_extension.dart
+++ lib/core/extensions/string_extension.dart
@@ -1,3 +1,11 @@
-extension StringExtension on String? {
+import 'package:flutter/material.dart';
+
+extension EmptyExtension on String? {
   bool get isEmptyOrNull => this == null || this!.isEmpty || this == '';
+}
+
+extension StringExtension on String {
+  Color get toColor {
+    return Color(int.parse(replaceAll("#", ""), radix: 16) + 0xFF000000);
+  }
 }
