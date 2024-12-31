b8eb4e9f5ec254bb112454d7ea2b74041d323bc4
Jewel <ylingzhu2022@gmail.com>
2024-12-31 17:02:30 +0800

    fix: Wait for ScreenUtil to initialize itself

--- lib/main.dart
+++ lib/main.dart
@@ -31,32 +31,46 @@ class MyApp extends StatelessWidget {
     }
   }
 
+  // 等待短暂的时间,确保ScreenUtil初始化完成,解决release模式下text不展示问题
+  // Wait for ScreenUtil to initialize itself.
+  Future initialiseScreenUtil() async {
+    await Future.delayed(Durations.short2);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(375, 812),
-      minTextAdapt: true,
-      splitScreenMode: true,
-      builder: (context, _) {
-        return GestureDetector(
-          onTap: () => unfocus(context),
-          child: MultiProvider(
-            providers: [
-              ChangeNotifierProvider(create: (_) => AppProvider()),
-            ],
-            child: MaterialApp(
-              title: 'Flutter Demo',
-              theme: AppTheme.lightTheme,
-              darkTheme: AppTheme.darkTheme,
-              themeMode: ThemeMode.dark,
-              onGenerateRoute: RouteConfig.generateRoute,
-              navigatorKey: navigatorKey,
-              localizationsDelegates: AppLocalizations.localizationsDelegates,
-              supportedLocales: AppLocalizations.supportedLocales,
-            ),
-          ),
-        );
-      },
-    );
+    return FutureBuilder(
+        future: initialiseScreenUtil(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SizedBox();
+          }
+          return ScreenUtilInit(
+            designSize: const Size(375, 812),
+            minTextAdapt: true,
+            splitScreenMode: true,
+            builder: (context, _) {
+              return GestureDetector(
+                onTap: () => unfocus(context),
+                child: MultiProvider(
+                  providers: [
+                    ChangeNotifierProvider(create: (_) => AppProvider()),
+                  ],
+                  child: MaterialApp(
+                    title: 'Flutter Demo',
+                    theme: AppTheme.lightTheme,
+                    darkTheme: AppTheme.darkTheme,
+                    themeMode: ThemeMode.dark,
+                    onGenerateRoute: RouteConfig.generateRoute,
+                    navigatorKey: navigatorKey,
+                    localizationsDelegates:
+                        AppLocalizations.localizationsDelegates,
+                    supportedLocales: AppLocalizations.supportedLocales,
+                  ),
+                ),
+              );
+            },
+          );
+        });
   }
 }
